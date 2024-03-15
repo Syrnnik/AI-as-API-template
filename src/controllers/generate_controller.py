1d2e2b37cd296a8a41d55d2b3c44ae77de8af282
Syrnnik <sakh.alexandr@gmail.com>
2024-03-15 09:25:56 +0300

    fix: generate controllers

--- src/controllers/generate_controller.py
+++ src/controllers/generate_controller.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 
-from src.models.generate_model import GenerateBody
+from services.whisper import tts_with_direct_model, tts_with_pipeline
 
 generate_router = APIRouter(tags=["AI"])
 
 
-@generate_router.post("/")
-def generate(body: GenerateBody):
-    pass
+@generate_router.post("/with-pipeline")
+def generate(uploaded_audio: UploadFile):
+    return tts_with_pipeline.get_text_from_audio_file(uploaded_audio)
+
+
+@generate_router.post("/directly")
+def generate(uploaded_audio: UploadFile):
+    return tts_with_direct_model.get_text_from_audio_file(uploaded_audio)
